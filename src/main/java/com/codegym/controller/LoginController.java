f37ea4e2604aa6a4167a7bebce5a0c844e712fd1
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-06-10 21:20:52 +0700

    d

--- src/main/java/com/codegym/controller/LoginController.java
+++ src/main/java/com/codegym/controller/LoginController.java
@@ -3,10 +3,7 @@ package com.codegym.controller;
 import com.codegym.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
@@ -17,49 +14,93 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class LoginController {
 
+//    @ModelAttribute
+//    public User setUpForm() {
+//        return new User();
+//    }
+//
+//    @RequestMapping
+//    public String Index(@CookieValue(value = "setUser", defaultValue = "") String setUser, Model model) {
+//        Cookie cookie = new Cookie("setUser", setUser);
+//        model.addAttribute("cookieValue", cookie);
+//        return "/login";
+//    }
+//
+//    @PostMapping("/dologin")
+//    public String doLogin(@ModelAttribute("user") User user, Model model, @CookieValue(value = "setUser", defaultValue = "")
+//            String setUser, HttpServletResponse response, HttpServletRequest request) {
+//        if (user.getEmail().equals("admin@gmail.com") && user.getPassword().equals("12345")) {
+//            if (user.getEmail() != null)
+//                setUser = user.getEmail();
+//
+//            Cookie cookie =new Cookie("setUser",setUser);
+//            cookie.setMaxAge(60);
+//            response.addCookie(cookie);
+//
+//            Cookie[]cookies = request.getCookies();
+//            for (Cookie ck:cookies){
+//                if (ck.getName().equals("setUser")){
+//                    model.addAttribute("cookieValue",ck);
+//                    break;
+//                }
+//                else {
+//                    ck.setValue("");
+//                    model.addAttribute("cookieValue",ck);
+//                    break;
+//                }
+//            }
+//            model.addAttribute("message","Login success. Welcome");
+//
+//        }else{
+//            user.setEmail("");
+//            Cookie cookie =new Cookie("setUser",setUser);
+//            model.addAttribute("cookieValue",cookie);
+//            model.addAttribute("message","Login Failed. Try again.");
+//        }
+//        return "/login";
+//    }
+
     @ModelAttribute
     public User setUpForm() {
         return new User();
     }
-
-    @RequestMapping
-    public String Index(@CookieValue(value = "setUser", defaultValue = "") String setUser, Model model) {
-        Cookie cookie = new Cookie("setUser", setUser);
-        model.addAttribute("cookieValue", cookie);
+//
+//    @GetMapping("/")
+//    public String getSigninForm() {
+//        return "/login";
+//    }
+        @RequestMapping
+    public String Index(@CookieValue(defaultValue = "") String email,
+                        @CookieValue(defaultValue = "") String password,Model model)
+        //Do ten cookie trung vs ten trong String nen co the ko viet value="password"
+    {
+        model.addAttribute("emailSave", email);
+        model.addAttribute("passwordSave", password);
         return "/login";
     }
 
-    @PostMapping("/dologin")
-    public String doLogin(@ModelAttribute("user") User user, Model model, @CookieValue(value = "setUser", defaultValue = "")
-            String setUser, HttpServletResponse response, HttpServletRequest request) {
+    @PostMapping("/signin")
+    public String doSignin(@ModelAttribute User user, HttpServletRequest httpServletRequest, Model model,
+                           HttpServletResponse httpServletResponse,@RequestParam("email")String email,
+                           @RequestParam("password")String password) {
+
+        user.setEmail(email);
+        user.setPassword(password);
         if (user.getEmail().equals("admin@gmail.com") && user.getPassword().equals("12345")) {
-            if (user.getEmail() != null)
-                setUser = user.getEmail();
+            Cookie emailSave = new Cookie("email", user.getEmail());
+            Cookie passwordSave = new Cookie("password", user.getPassword());
 
-            Cookie cookie =new Cookie("setUser",setUser);
-            cookie.setMaxAge(60);
-            response.addCookie(cookie);
+            httpServletResponse.addCookie(emailSave);
+            httpServletResponse.addCookie(passwordSave);
+            model.addAttribute("emailSave",email);
+            model.addAttribute("passwordSave",password);
+            model.addAttribute("message", "Login success. Welcome");
 
-            Cookie[]cookies = request.getCookies();
-            for (Cookie ck:cookies){
-                if (ck.getName().equals("setUser")){
-                    model.addAttribute("cookieValue",ck);
-                    break;
-                }
-                else {
-                    ck.setValue("");
-                    model.addAttribute("cookieValue",ck);
-                    break;
-                }
-            }
-            model.addAttribute("message","Login success. Welcome");
-
-        }else{
-            user.setEmail("");
-            Cookie cookie =new Cookie("setUser",setUser);
-            model.addAttribute("cookieValue",cookie);
-            model.addAttribute("message","Login Failed. Try again.");
+            return "/login";
+        } else {
+            model.addAttribute("message", "Login Failed. Try again.");
         }
-        return "/login";
+        return "/fail";
     }
+
 }
